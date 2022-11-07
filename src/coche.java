c49652cda467f78df626776737a3dc1d5f3f30ad
select <elyshd@gmail.com>
2022-11-06 22:52:08 -0300

    mi 2do commit

--- src/coche.java
+++ src/coche.java
@@ -1,2 +1,28 @@
-package PACKAGE_NAME;public class coche {
+
+
+
+public class coche {
+    public static void main(String[] args) {
+
+        Coche miCoche = new Coche();
+        miCoche.AñadirPuerta();
+        miCoche.AñadirPuerta();
+        miCoche.AñadirPuerta();
+        miCoche.AñadirPuerta();
+        System.out.println(miCoche.puertas);
+    }
+    public static int sumaParams(int a, int b, int c) {
+        return a + b + c;
+    }
+    static class Coche {
+
+        public int puertas = 0;
+
+        public void AñadirPuerta() {
+            this.puertas++;
+        }
+    }
+
+
+
 }
---
+++ src/main.java
@@ -0,0 +1,14 @@
+
+  //programacion orienta a objeto
+public class main {
+
+    public static void main(String[] args) {
+         suma(78, 87, 187);
+    }
+
+    public static void suma(int a, int b, int c) {
+        System.out.println("La suma de:"+(a + b + c));
+
+
+    }
+}
